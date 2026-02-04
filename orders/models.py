798ff6d3646d7b17367b4335c47eb9e52215888d
Mohammad Marouful Islam Zishan <87011713+zishan344@users.noreply.github.com>
2026-02-04 19:11:35 +0600

    66134

--- orders/models.py
+++ orders/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 # Create your models here.
 class MenuCategory(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, unique=True, verbose_name="Category name")
