fdcf4f95c38c61471cc919c39f1599e27a43df13
Mohammad Marouful Islam Zishan <87011713+zishan344@users.noreply.github.com>
2026-02-04 19:15:32 +0600

    66134

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 
+from django.db import models
 # Create your models here.
+# Menue category mdoel for food manue
 class MenuCategory(models.Model):
     name = models.CharField(max_length=255, unique=True, verbose_name="Category name")
