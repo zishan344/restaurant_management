37a6d278403df97e4e238fd24b0ce94fd19d4064
Mohammad Marouful Islam Zishan <87011713+zishan344@users.noreply.github.com>
2026-02-05 16:32:47 +0600

    66138

---
+++ home/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ModelSerializer
+from home.models import MenuCategory
+
+class MenuCategorySerializer(ModelSerializer):
+    class Meta:
+        model = MenuCategory
+        fields = ["name"]
