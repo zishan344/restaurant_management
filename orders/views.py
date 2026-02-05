51716ed5a2b242f862162108c6938951a0c701a7
Mohammad Marouful Islam Zishan <87011713+zishan344@users.noreply.github.com>
2026-02-05 13:23:47 +0600

    66137

--- orders/views.py
+++ orders/views.py
@@ -1,34 +1,33 @@
-from urllib import request, response
+from django.shortcuts import render
 from rest_framework import status
-from rest_framework.views import APIView 
+from rest_framework.ApiView import ApiView
 from rest_framework.response import Response
 from orders.models import Coupon
 from orders.utils import validateDate
 
-class CouponValidationView(APIView):
+# Create your views here.
+
+class CouponValidationView(ApiView):
     def post(self, request):
-        code = request.data.get('code')
+        code = request.data.get()
         if not code:
-            return Response(
-                {"error":"The 'code' field is required"},
-                status=status.HTTP_400_BAD_REQUEST
-            )
+            return Response({
+                "error":"The 'code' field is required"
+            }, status=status.HTTP_400_BAD_REQUEST)
         queryset = Coupon.objects.filter(code=code)
-        
         if queryset.exists():
             data = queryset.first()
             is_active = data.is_active
-        
-            if is_active and validateDate(data.valid_from, data.valid_until):
-                return Response(
-                    {"valid":True, "message":"applied coupon successfully"},status=status.HTTP_200_OK
-                )
-            else:
-                return Response(
-                    {"valid":False, "error":"invalid coupon"},status=status.HTTP_200_OK
-                )
-        return Response({"valid":False, "error":"Invalid coupon code"}, status=status.HTTP_404_NOT_FOUND)
+            if(is_active and validateDate(
+                data.valid_from,data.valid_until)):
+                return Response({"valid":True, "message":"applied coupon successfully"},
+                status=status.HTTP_200_OK)
+            return Response({
+                "valid":False,
+                "error":"Invalid coupon"
+            },status=HTTP_400_BAD_REQUEST)
+        return Response({
+            "valid":False,
+            "error":"Invalid coupon code"
+        }, status=status.HTTP_404_NOT_FOUND)
 
-
-
-        
