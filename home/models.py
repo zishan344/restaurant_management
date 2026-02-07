ccac5c3f1cbbee90d237320a4252498b21db30fe
zishan344 <zishanahmed344@gmail.com>
2026-02-07 16:07:43 +0600

    reupdate restaurant model

--- home/models.py
+++ home/models.py
@@ -2,51 +2,6 @@ from django.db import models
 # Create your models here.
 # Menue category mdoel for food menu
 
-class Restaurant(models.Model):
-    # write your code here
-    name = models.CharField(
-        max_length=100,
-        null=False,
-        blank=False,
-        verbose_name="RestaurantName"
-    )
-    owner_name = models.CharField(
-        max_length=100,
-        null=False,
-        blank=False,
-        verbose_name="Name"
-    )
-    email = models.EmailField(
-        max_length=250,
-        null=False,
-        blank=False,
-        verbose_name="Email"
-    )
-    phone_number = models.CharField(
-        max_length=50,
-        null=False,
-        blank=False,
-        verbose_name="ContactNumber"
-    )
-    address = models.TextField(
-        null=False,
-        blank=False,
-        verbose_name="Address"
-    )
-    city = models.CharField(
-        max_length=100,
-        null=False,
-        blank=False,
-        verbose_name="City"
-    )
-    created_at = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name="created time"
-    )
-    class Meta:
-        # default ordering
-        ordering = ['-created_at']
-
 
 class MenuCategory(models.Model):
     name = models.CharField(max_length=255, unique=True, verbose_name="CategoryName")
