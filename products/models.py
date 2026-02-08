531f3db15a1f1e67ac91717853e5b854b8f796e7
zishan344 <zishanahmed344@gmail.com>
2026-02-08 18:15:24 +0600

    implement best practice for menue item

--- products/models.py
+++ products/models.py
@@ -1,10 +1,40 @@
 from django.db import models
-
+from account.models import Restaurant
 # Create your models here.
-class Item(models.Model):
-    item_name = models.CharField(max_length=150)
-    item_price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
+from django.db import models
+from .models import Restaurant  # Make sure this import path is correct
 
+class MenuItem(models.Model):
+    ''' 
+    restaurant menu item
+    '''
+    restaurant = models.ForeignKey(
+        Restaurant,
+        on_delete=models.CASCADE,
+        related_name="menu_items", 
+        verbose_name="restaurant"      
+        )
+    name = models.CharField(
+        max_length=150,
+        verbose_name = "item_name",
+        )
+    description = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name="item_description"
+        )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2, 
+        verbose_name="price"
+        )
+    is_available = models.BooleanField(
+        default=True,
+        verbose_name="available_item")
+    created_at = models.DateTimeField(
+        auto_now_add=True, 
+        verbose_name="created_date"
+        )
+    
     def __str__(self):
-        return str(self.item_name)
+        return str(self.name)
