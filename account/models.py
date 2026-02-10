b0fa510d8941b43c6784d875969104779ef799a9
zishan344 <zishanahmed344@gmail.com>
2026-02-10 20:39:58 +0600

    add restaurant model

--- account/models.py
+++ account/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Restaurant(models.Model):
-    # write your code here
+    """ Restaurnt Model """
     name = models.CharField(
         max_length=100,
         null=False,
